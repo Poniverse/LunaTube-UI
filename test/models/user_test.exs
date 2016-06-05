8accf2b321cd4b541d2fb882c35c2584a78c162b
Codesign <cs@csdev.io>
2016-06-05 19:31:36 +0100

    Added user model

---
+++ test/models/user_test.exs
@@ -0,0 +1,18 @@
+defmodule LunaTube.UserTest do
+  use LunaTube.ModelCase
+
+  alias LunaTube.User
+
+  @valid_attrs %{display_name: "some content", email: "some content", username: "some content"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = User.changeset(%User{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+end
